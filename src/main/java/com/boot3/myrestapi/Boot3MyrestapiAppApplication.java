5037f96f1a98518de0aea6d7086a7c79036cab9f
mysoyul <vega2k_@naver.com>
2023-05-15 14:32:25 +0900

    modelmapper bean 설정

--- src/main/java/com/boot3/myrestapi/Boot3MyrestapiAppApplication.java
+++ src/main/java/com/boot3/myrestapi/Boot3MyrestapiAppApplication.java
@@ -1,7 +1,9 @@
 package com.boot3.myrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Boot3MyrestapiAppApplication {
@@ -10,4 +12,9 @@ public class Boot3MyrestapiAppApplication {
 		SpringApplication.run(Boot3MyrestapiAppApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
 }
