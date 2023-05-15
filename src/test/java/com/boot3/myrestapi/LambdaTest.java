21e4edd8371dc1d8b4d1b7cae438d4ede6db3a2a
mysoyul <vega2k_@naver.com>
2023-05-15 17:50:12 +0900

    LambdaTest update

--- src/test/java/com/boot3/myrestapi/LambdaTest.java
+++ src/test/java/com/boot3/myrestapi/LambdaTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class LambdaTest {
     @Test
     public void runnable() {
-        //1. Anonymous Inner class
+        //1. Anonymous Inner class 익명 내부 클래스
         Thread t1 = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -17,14 +18,15 @@ public class LambdaTest {
             }
         });
         t1.start();
+
         //2. Lambda Expression
         Thread t2 = new Thread(() -> System.out.println("Lambda Expression"));
         t2.start();
     }
     
-    @Test @Disabled
+    @Test //@Disabled
     public void consumer() {
-        List<String> list = List.of("aa", "bb", "cc");//Immutable List
+        List<String> list = List.of("aa", "bb", "cc"); //Immutable List
         //1. Anonymous Inner class
         list.forEach(new Consumer<String>() {
             @Override
@@ -32,11 +34,28 @@ public class LambdaTest {
                 System.out.println("s = " + s);
             }
         });
+
         //2.Lambda Expression
         //Consumer의 추상 메서드 void accept(T t)
-        list.forEach(val -> System.out.println(val));
-        //3.Method Reference
+        list.forEach(val -> System.out.println(" >>> " + val));
+
+        //3.Method Reference (람다식을 simple하게 표현)
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void predicate_operator() {
+        List<Integer> integerList = List.of(10, 13, 20, 35, 40, 60, 55);
+
+        integerList.stream()//Stream<Integer>
+                .filter(val -> val % 2 == 0)
+                .forEach(System.out::println);
+        //2의 배수 합계
+        int sum = integerList.stream()
+                .mapToInt(Integer::intValue)//IntStream
+                .sum();
+        System.out.println("sum = " + sum);
+
     }
 
 }
