652b2949face8a5ecd52b5e6781ce4d4920f8da2
mysoyul <vega2k_@naver.com>
2023-05-16 14:18:35 +0900

    UserRepository add

---
+++ src/main/java/com/boot3/myrestapi/userinfo/UserInfoRepository.java
@@ -0,0 +1,8 @@
+package com.boot3.myrestapi.userinfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String username);
+}
